7c6fd9dacb2b773efde0b385ea600718237c19d7
malejartunduaga <93880501+malejartunduaga@users.noreply.github.com>
2021-11-07 13:30:09 -0500

    Primer commit

---
+++ scripts.js
@@ -0,0 +1,11 @@
+function alertHelloWorld() {
+    const message = 'Hola mundo!';
+
+    console.log('[INFO] Mi página web dice: ', message);
+}
+
+function alertHelloWorld() {
+    const message = 'Hola mundo!';
+
+    // alert(message);
+}
